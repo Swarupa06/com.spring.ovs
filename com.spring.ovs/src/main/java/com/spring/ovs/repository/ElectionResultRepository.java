532647e648000631bc067108f445fc4ac54226c4
Swarupa06 <74255053+Swarupa06@users.noreply.github.com>
2020-11-11 23:23:47 +0530

    Update ElectionResultRepository.java

--- com.spring.ovs/src/main/java/com/spring/ovs/repository/ElectionResultRepository.java
+++ com.spring.ovs/src/main/java/com/spring/ovs/repository/ElectionResultRepository.java
@@ -1,39 +1,36 @@
-package com.spring.ovs.repository;
+package com.ovs.spring.demo;
 
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import com.spring.ovs.dtos.ElectionResult;
-import com.spring.ovs.dtos.NominatedCandidates;
-
 
 public interface ElectionResultRepository extends JpaRepository<ElectionResult, Integer> {
 
 	
-	@Query("select er.totalPollingPercentage from results er")
+	@Query("select er.totalPollingPercentage from ElectionResult er")
 	public double viewVotingPercentage();
 	
-	@Query("select er.candidateVotesPercentage from results er where er.candidateId=24")
+	@Query("select er.candidateVotesPercentage from ElectionResult er, NominatedCandidates nc where er.id = nc.candidateId")
 	public double viewCandidateVotingPercent(int candidateId);
 
-	@Query("select er.coop_SocietyName,er.totalSocietyVotes,er.totalCandidateVotes,er.totalPolledVotes,er.candidateVotesPercentage,er.totalPollingPercentage,er.result from results er")
+	@Query("select er.coop_SocietyName,er.totalSocietyVotes,er.totalCandidateVotes,er.totalPolledVotes,er.candidateVotesPercentage,er.totalPollingPercentage,er.result from ElectionResult er")
 	public void displayVotingStatistics();
 	
-	@Query("select MAX(er.candidateVotesPercentage) from results er")
+	@Query("select MAX(er.candidateVotesPercentage) from ElectionResult er")
 	public  NominatedCandidates viewHighestVotingPercentCandidate();
 	
-	@Query("select MIN(er.candidateVotesPercentage) from results er")
+	@Query("select MIN(er.candidateVotesPercentage) from ElectionResult er")
 	public  NominatedCandidates viewLowestVotingPercentCandidate();
 	
-	@Query("select COUNT(er.id) from results er GROUP BY er.id HAVING COUNT(er.id)>1")
+	@Query("select COUNT(er.id) from ElectionResult er GROUP BY er.id HAVING COUNT(er.id)>1")
 	public int viewInvalidVotes();
 	
-	@Query("select COUNT(er.candidateId) from results er GROUP BY er.candidateId HAVING COUNT(er.candidateId)>1")
+	@Query("select COUNT(er.candidate) from ElectionResult er,NominatedCandidates nc where er.id = nc.candidateId GROUP BY er.candidate HAVING COUNT(er.candidate)>1")
 	public  List<NominatedCandidates> candidatewiseInvalidVotesList();
 	
-	@Query("select er.result from results er")
+	@Query("select er.result from ElectionResult er")
 	public void displayPollingResult();
 
 }
