8dc9e37b0acdf1786942b4a7f515d84edb05a70d
Swarupa06 <74255053+Swarupa06@users.noreply.github.com>
2020-11-11 23:26:39 +0530

    Update ElectionOfficerTest.java

--- com.spring.ovs/src/test/java/com/spring/swagger/ovs/ElectionOfficerTest.java
+++ com.spring.ovs/src/test/java/com/spring/swagger/ovs/ElectionOfficerTest.java
@@ -1,79 +1,38 @@
-package com.spring.swagger.ovs;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import java.util.ArrayList;
-import java.util.List;
-
+package com.ovs.spring.demo;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
 
 
-import com.spring.ovs.controllers.ElectionOfficerController;
-import com.spring.ovs.dtos.ElectionOfficer;
-import com.spring.ovs.exceptions.ElectionOfficerNotFoundException;
-import com.spring.ovs.service.ElectionOfficerService;
 
-
-@ExtendWith(MockitoExtension.class)
 public class ElectionOfficerTest
 {
-    
-	@Mock
-	ElectionOfficerService eoServices;
+  ElectionOfficerService service=mock(ElectionOfficerService.class);
+  ElectionOfficer officer=new ElectionOfficer();
 	
-
-	@InjectMocks
-	ElectionOfficerController eoController;
+	@Test
+	void testAdd() 
+	{
+		
+		when(service.addElectionOfficerDetails(officer)).thenReturn(officer.getOfficerId());
+		assertEquals(officer.getOfficerId(),service.addElectionOfficerDetails(officer));
+		
+	}
 
 	@Test
-	public void testAddElectionOfficerDetails() 
+	void testUpdate() 
 	{
-		 ElectionOfficer officer =new ElectionOfficer();
-		Mockito.when(eoController.addElectionOfficerDetails(officer)).thenReturn(officer);
-		assertEquals(officer,eoController.addElectionOfficerDetails(officer));
-	 }
-	
-	 @Test
-	 public void testGetAllElectionOfficers()
-     {
-	     List<ElectionOfficer> officer=new ArrayList<ElectionOfficer>();
-	     Mockito.when(eoController.getAllElectionOfficers()).thenReturn(officer);
-	     assertEquals(officer,eoController.getAllElectionOfficers());
-
-	  }
-	 
-	 @Test
-	 public void testElectionOfficerById() throws ElectionOfficerNotFoundException
-	 {
-		 ElectionOfficer officer=new ElectionOfficer();
-		 int officerId = 1;
-		Mockito.when(eoController.viewElectionOfficerById(officerId)).thenReturn(officer);
-	     assertEquals(officer,eoController.viewElectionOfficerById(officerId)); 
-	 }
-	 
-	 @Test
-	 public void testUpdateElectionOfficerDetails()throws ElectionOfficerNotFoundException
-	 {
-		 ElectionOfficer officer=new ElectionOfficer();
 		int officerId = 1;
-		Mockito.when(eoController.updateElectionOfficerDetails(officerId, officer)).thenReturn(officer);
-		assertEquals(officer,eoController.updateElectionOfficerDetails(officerId, officer));
+		when(service.updateElectionOfficerDetails(officer)).thenReturn(officerId);
+		assertEquals(officerId,service.updateElectionOfficerDetails(officer));
+	}
 
-	 }
-	 
-	 @Test
-	 public void testDeleteElectionOfficer() throws ElectionOfficerNotFoundException
-	 {
-	 	
-	 	int officerId = 1;
-		Mockito.when(eoController.deleteElectionOfficer(officerId)).thenReturn(officerId);
-	 	assertEquals(officerId,eoController.deleteElectionOfficer(officerId));
-	 	
-	 }
+	@Test
+	void testDelete()
+	{
+		int officerId = 1;
+		when(service.deleteElectionOfficer(officerId)).thenReturn(officerId);
+		assertEquals(officerId,service.deleteElectionOfficer(officerId));
+	}
 }
-
