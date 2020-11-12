cc4094b0fc58e14f24c3ef62fb2c2866efe2219d
sirisha696 <72748365+sirisha696@users.noreply.github.com>
2020-11-12 19:05:16 +0530

    Update User.java

--- com.spring.ovs/src/main/java/com/spring/ovs/dtos/User.java
+++ com.spring.ovs/src/main/java/com/spring/ovs/dtos/User.java
@@ -6,6 +6,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+//import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
 
 @Entity
 @Table(name="userdetails")
@@ -15,23 +19,34 @@ public class User{
 
 	@Id
 	@GeneratedValue(strategy= GenerationType.IDENTITY)
-	@Column(name="userId")
+	@Column(name="user_id",unique=true)
 	private int userId;
 	
-	@Column(name="password")
+	@NotEmpty(message = "Password is required")
+	//@Pattern(regexp="^(?=.[A-Za-z])(?=.\\d)[A-Za-z\\d]{8,}$")
+	//@Pattern(regexp="[a-zA-Z]{5,}@&{1}")
+	@Column(name = "password")
  	private String password;
  	
-	
- 	@Column(name="firstName")
+
+	@NotEmpty(message = "First name is required")
+	@Pattern(regexp="^[A-Za-z]{3,}$")
+	@Column(name = "first_name")
  	private String firstName;
  	
- 	@Column(name="lastName")
+	@NotEmpty(message = "Last name is required")
+	@Pattern(regexp="^[A-Za-z]{3,}$")
+	@Column(name = "last_name")
  	private String lastName;
- 	
- 	@Column(name="email")
+	
+	@NotEmpty(message = "Email Id is required")
+	@Pattern(regexp="^[a-zA-Z]{1}[a-zA-Z0-9]{2,}@[a-z]{5}.com$",message="Email address is invalid")
+	@Column(name = "email",unique=true)
  	private String email;
- 	
- 	@Column(name="contactno")
+	
+	@NotEmpty(message = "Contact number is required")
+	@Pattern(regexp="^[6-9]{1}[0-9]{9}$")
+	@Column(name = "contact_no",unique=true)
  	private String contactno;
  	
  	
@@ -82,6 +97,7 @@ public class User{
 		this.password=password;
 		this.firstName=firstName;
 		this.lastName=lastName;
+		this.email=email;
 		this.contactno=contactno;
 	}
 	
@@ -91,6 +107,7 @@ public class User{
 		this.password=password;
 		this.firstName=firstName;
 		this.lastName=lastName;
+		this.email=email;
 		this.contactno=contactno;
 	}
 	@Override
