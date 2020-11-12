d70ae4e2f3513293aa00ba9148c1aeac89869687
Swarupa06 <74255053+Swarupa06@users.noreply.github.com>
2020-11-12 17:41:25 +0530

    Update ElectionResult.java

--- com.spring.ovs/src/main/java/com/spring/ovs/dtos/ElectionResult.java
+++ com.spring.ovs/src/main/java/com/spring/ovs/dtos/ElectionResult.java
@@ -9,9 +9,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
-//import javax.persistence.JoinTable;
-//import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -29,100 +30,130 @@ public class ElectionResult {
 	private Date pollingDate;
 	
 	@OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "candidateId")
+        @JoinColumn(name = "candidateId")
 	private NominatedCandidates candidate;
     
+	@NotEmpty(message = "Society name is required")
+	@Pattern(regexp="^[A-Za-z]{3,}$")
 	@Column(name = "coop_societyName")
 	private String coop_SocietyName;
 	
+	@NotNull(message = "Please enter total society votes")
 	@Column(name = "totalSocietyVotes")
 	private int totalSocietyVotes;
 	
+	@NotNull(message = "Please enter total polled votes")
 	@Column(name = "totalPolledVotes")
 	private int totalPolledVotes;
 	
+	@NotNull(message = "Please enter total polling percentage")
 	@Column(name = "totalPollingPercentage")
 	private float totalPollingPercentage;
 	
+	@NotNull(message = "Please enter total candidate votes")
 	@Column(name = "totalCandidateVotes")
 	private int totalCandidateVotes;
 	
+	@NotNull(message = "Please enter candidate votes percentage")
 	@Column(name = "candidateVotesPercentage")
 	private float candidateVotesPercentage;
 	
+	@NotEmpty(message = "Result is required")
 	@Column(name = "result")
 	private String result;
 	
-	public int getId() {
+	public int getId() 
+	{
 		return id;
 	}
-	public void setId(int id) {
+	public void setId(int id)
+	{
 		this.id = id;
 	}
-	public Date getPollingDate() {
+	public Date getPollingDate()
+	{
 		return pollingDate;
 	}
-	public void setPollingDate(Date pollingDate) {
+	public void setPollingDate(Date pollingDate)
+	{
 		this.pollingDate = pollingDate;
 	}
 	
 	
-	public NominatedCandidates getCandidate() {
+	public NominatedCandidates getCandidate() 
+	{
 		return candidate;
 	}
-	public void setCandidate(NominatedCandidates candidate) {
+	public void setCandidate(NominatedCandidates candidate) 
+	{
 		this.candidate = candidate;
 	}
-	public String getCoop_SocietyName() {
+	public String getCoop_SocietyName()
+	{
 		return coop_SocietyName;
 	}
-	public void setCoop_SocietyName(String coop_SocietyName) {
+	public void setCoop_SocietyName(String coop_SocietyName)
+	{
 		this.coop_SocietyName = coop_SocietyName;
 	}
-	public int getTotalSocietyVotes() {
+	public int getTotalSocietyVotes() 
+	{
 		return totalSocietyVotes;
 	}
-	public void setTotalSocietyVotes(int totalSocietyVotes) {
+	public void setTotalSocietyVotes(int totalSocietyVotes)
+	{
 		this.totalSocietyVotes = totalSocietyVotes;
 	}
-	public int getTotalPolledVotes() {
+	public int getTotalPolledVotes()
+	{
 		return totalPolledVotes;
 	}
-	public void setTotalPolledVotes(int totalPolledVotes) {
+	public void setTotalPolledVotes(int totalPolledVotes)
+	{
 		this.totalPolledVotes = totalPolledVotes;
 	}
-	public float getTotalPollingPercentage() {
+	public float getTotalPollingPercentage()
+	{
 		return totalPollingPercentage;
 	}
-	public void setTotalPollingPercentage(float totalPollingPercentage) {
+	public void setTotalPollingPercentage(float totalPollingPercentage)
+	{
 		this.totalPollingPercentage = totalPollingPercentage;
 	}
-	public int getTotalCandidateVotes() {
+	public int getTotalCandidateVotes()
+	{
 		return totalCandidateVotes;
 	}
-	public void setTotalCandidateVotes(int totalCandidateVotes) {
+	public void setTotalCandidateVotes(int totalCandidateVotes)
+	{
 		this.totalCandidateVotes = totalCandidateVotes;
 	}
-	public float getCandidateVotesPercentage() {
+	public float getCandidateVotesPercentage()
+	{
 		return candidateVotesPercentage;
 	}
-	public void setCandidateVotesPercentage(float candidateVotesPercentage) {
+	public void setCandidateVotesPercentage(float candidateVotesPercentage)
+	{
 		this.candidateVotesPercentage = candidateVotesPercentage;
 	}
-	public String getResult() {
+	public String getResult()
+	{
 		return result;
 	}
-	public void setResult(String result) {
+	public void setResult(String result)
+	{
 		this.result = result;
 	}
 	
 
-	public ElectionResult() {
+	public ElectionResult()
+	{
 		super();
 	}
 	public ElectionResult(Date pollingDate, NominatedCandidates candidate, String coop_SocietyName,
 			int totalSocietyVotes, int totalPolledVotes, float totalPollingPercentage, int totalCandidateVotes,
-			float candidateVotesPercentage, String result) {
+			float candidateVotesPercentage, String result) 
+	{
 		super();
 		this.pollingDate = pollingDate;
 		this.candidate = candidate;
@@ -137,7 +168,8 @@ public class ElectionResult {
 	
 	public ElectionResult(int id, Date pollingDate, NominatedCandidates candidate, String coop_SocietyName,
 			int totalSocietyVotes, int totalPolledVotes, float totalPollingPercentage, int totalCandidateVotes,
-			float candidateVotesPercentage, String result) {
+			float candidateVotesPercentage, String result)
+	{
 		super();
 		this.id = id;
 		this.pollingDate = pollingDate;
@@ -151,21 +183,10 @@ public class ElectionResult {
 		this.result = result;
 	}
 	
-	public ElectionResult(int id, String coop_SocietyName, int totalSocietyVotes,
-			int totalPolledVotes, float totalPollingPercentage, int totalCandidateVotes,
-			float candidateVotesPercentage, String result)
-	{
-		this.id = id;
-		this.coop_SocietyName = coop_SocietyName;
-		this.totalSocietyVotes = totalSocietyVotes;
-		this.totalPolledVotes = totalPolledVotes;
-		this.totalPollingPercentage = totalPollingPercentage;
-		this.totalCandidateVotes = totalCandidateVotes;
-		this.candidateVotesPercentage = candidateVotesPercentage;
-		this.result = result;
 	}
 	@Override
-	public String toString() {
+	public String toString()
+	{
 		return "ElectionResult [id=" + id + ", pollingDate=" + pollingDate + ", candidate=" + candidate
 				+ ", coop_SocietyName=" + coop_SocietyName + ", totalSocietyVotes=" + totalSocietyVotes
 				+ ", totalPolledVotes=" + totalPolledVotes + ", totalPollingPercentage=" + totalPollingPercentage
