017700d3b7e35fec21cb4e33d12b8462b628730c
Swarupa06 <74255053+Swarupa06@users.noreply.github.com>
2020-11-12 17:51:14 +0530

    Update ElectionResultService.java

--- com.spring.ovs/src/main/java/com/spring/ovs/service/ElectionResultService.java
+++ com.spring.ovs/src/main/java/com/spring/ovs/service/ElectionResultService.java
@@ -1,114 +1,127 @@
 package com.ovs.spring.demo;
 
-
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.spring.swagger.ovs.dao.ElectionOfficerDao;
+import com.spring.swagger.ovs.dtos.ElectionOfficer;
+import com.spring.swagger.ovs.exceptions.ElectionOfficerNotFoundException;
+
 
 @Service
-public class ElectionResultService {
-    
+public class ElectionOfficerService {
+
 	@Autowired
-	ElectionResultRepository erRepo;
+	private ElectionOfficerDao elDao;
 	
-	public int addElectionResult(ElectionResult result)
-	{
-		erRepo.save(result);
-		return result.getId();
-	}
 	
-	public List<ElectionResult> viewElectionResultList()
+	/*
+	  * This is the method for adding Election officer details.
+	 */
+
+	public int addElectionOfficerDetails(ElectionOfficer officer)
 	{
-		List<ElectionResult> result = new ArrayList<ElectionResult>(); 
-		System.out.println("Election Results...");
-		erRepo.findAll().forEach(result1 -> result.add(result1));
-     	return result;	
-	}
-	
-	public ElectionResult viewCandidatewiseResult(int id)
-	{
-		
-		return erRepo.findById(id).get();	
-		
-	}
-	
-	public double viewVotingPercentage()
-	{
-		
-		return erRepo.viewVotingPercentage();	
-		
-	}
-	
-	public double viewCandidateVotingPercent(int candidateId)
-	{
-		
-		return erRepo.viewCandidateVotingPercent(candidateId);	
-		
-	}
-	
-	public void displayVotingStatistics()
-	{
-		
-		erRepo.displayVotingStatistics();
-		
-	}
-	
-	public  NominatedCandidates viewHighestOrLowestVotingPercentCandidate(boolean H)
-	{
-		
-		System.out.println("Enter the character H to view the Highest voting percentage candidate or It will display the Lowest voting percentage candidate");
-		@SuppressWarnings("resource")
-		Scanner sc=new Scanner(System.in);
-		sc.nextBoolean();
-		if (H==true)
-		{	
-		 return erRepo.viewHighestVotingPercentCandidate();
-	    }
+		if(elDao.existsById(officer.getOfficerId()))
+		{
+			System.out.println("The Officer with this Id already exists...Give another value");
+		     return 0;
+		}
 		else
 		{
-			return erRepo.viewLowestVotingPercentCandidate();
+		   return elDao.save(officer).getOfficerId();
 		}
+			 
 	}
 	
-	public  NominatedCandidates viewHighestVotingPercentCandidate() 
-	{
-		
-		return erRepo.viewHighestVotingPercentCandidate();
-		
-	}
 	
-	public  NominatedCandidates viewLowestVotingPercentCandidate() 
-	{
-		
-		return erRepo.viewLowestVotingPercentCandidate();
-		
-	}
+	 /*
+	   * This is the method for Updating the existing Election officer details. 
+	 */
 	
-	public int viewInvalidVotes()
-	{
-		
-		return erRepo.viewInvalidVotes();
-		
-	}
 	
-	public  List<NominatedCandidates> candidatewiseInvalidVotesList()
-	{
-		
-		return erRepo.candidatewiseInvalidVotesList();
-		
-	}
-	
-	public void displayPollingResult()
-	{
-		
-		erRepo.displayPollingResult();
-		
-	}
+    public ElectionOfficer updateElectionOfficerDetails(int officerId, ElectionOfficer officer) throws ElectionOfficerNotFoundException
+    {
 
-	
-	
+	      Optional<ElectionOfficer> repOfficer = elDao.findById(officerId);
+
+	        if (!repOfficer.isPresent()) 
+		{
+
+		    ElectionOfficer officerToBeUpdated = repOfficer.get();
+		    officerToBeUpdated.setFirstName(officer.getFirstName());
+		    officerToBeUpdated.setLastName(officer.getLastName());
+		    officerToBeUpdated.setPassword(officer.getPassword());
+		    officerToBeUpdated.setGender(officer.getGender());
+		    officerToBeUpdated.setMobileno(officer.getMobileno());
+		    officerToBeUpdated.setEmailId(officer.getEmailId());
+		    officerToBeUpdated.setAddress1(officer.getAddress1());
+		    officerToBeUpdated.setAddress2(officer.getAddress2());
+		    officerToBeUpdated.setDistrict(officer.getDistrict());
+		    officerToBeUpdated.setPincode(officer.getPincode());
+		 
+		    return elDao.save(officerToBeUpdated);
+	      }
+	      else
+	      {
+		 throw new ElectionOfficerNotFoundException("No Officer present with the given Id to Update...Please enter valid Id to update");
+
+	      }
+      }
+    
+      
+     /*
+        * This is the method for deleting the existing Election officer details.
+     */
+    
+    
+     public int deleteElectionOfficer(int officerId) throws ElectionOfficerNotFoundException
+     {
+    	 Optional<ElectionOfficer> repOfficer = elDao.findById(officerId);
+    		 
+    	  if(repOfficer.isPresent())
+    	   {
+    	      elDao.deleteById(officerId);
+              System.out.println("Officer details are deleted Successfully");
+    		}
+    	    else
+    		{
+    		    throw new ElectionOfficerNotFoundException("Enter valid Id to be deleted");
+    		}
+		    return officerId;
+      }
+     
+     /*
+        * This method is used to view the particular Election officer details with the entered ID.
+      */
+    
+	 public ElectionOfficer viewElectionOfficerById(int officerId) throws ElectionOfficerNotFoundException
+	 {
+	      Optional<ElectionOfficer> repOfficer = elDao.findById(officerId);
+	      
+	      if(repOfficer.isPresent())
+	      {
+			 return repOfficer.get();
+	      }
+	      else
+	      {
+  		    throw new ElectionOfficerNotFoundException("Enter valid Id to view the Officer details"); 
+	      }
+		
+	 }
+	 
+	 /*
+	    * This method is used to get all the existing Election officers Details.
+	  */
+	 
+	 public List<ElectionOfficer> getAllElectionOfficers()
+	 {
+	 
+		System.out.println("Election Officers...");
+		return elDao.findAll();
+	 }
 }
+
+
