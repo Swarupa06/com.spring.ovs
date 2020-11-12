081c9dab2bfec0304f7d03e63614ab7a11dadead
Swarupa06 <74255053+Swarupa06@users.noreply.github.com>
2020-11-12 08:53:30 +0530

    Add files via upload

---
+++ com.spring.ovs/src/main/java/com/spring/ovs/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.ovs.spring.demo;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+	@ExceptionHandler(DuplicateRecordException.class)
+    @ResponseStatus(value=HttpStatus.NOT_FOUND)
+    public ResponseEntity<Object> resouceNotFoundException(DuplicateRecordException ex, WebRequest request) {
+    	ExceptionDetails exceptionDetails = new ExceptionDetails(new Date(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(exceptionDetails, HttpStatus.NOT_FOUND);
+    }
+    
+	 @ExceptionHandler(ElectionOfficerNotFoundException.class)
+	    @ResponseStatus(value=HttpStatus.NOT_FOUND)
+	    public ResponseEntity<Object> resouceNotFoundException(ElectionOfficerNotFoundException ex, WebRequest request) {
+	    	ExceptionDetails exceptionDetails = new ExceptionDetails(new Date(), ex.getMessage(), request.getDescription(false));
+	    	System.out.println("1");
+	        return new ResponseEntity<>(exceptionDetails, HttpStatus.NOT_FOUND);
+	    }
+}
