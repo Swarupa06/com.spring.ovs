d62f182fd9e4f231e6c5d22f9696aab5f77aca63
satya6666-del <72726749+satya6666-del@users.noreply.github.com>
2020-11-12 19:12:03 +0530

    Update NominatedCandidatesTest.java

--- com.spring.ovs/src/test/java/com/spring/swagger/ovs/NominatedCandidatesTest.java
+++ com.spring.ovs/src/test/java/com/spring/swagger/ovs/NominatedCandidatesTest.java
@@ -11,7 +11,8 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import com.spring.ovs.controllers.NominatedConditesController;
+import com.spring.ovs.controllers.NominatedCandidatesController;
+
 import com.spring.ovs.dtos.NominatedCandidates;
 import com.spring.ovs.exceptions.NominatedCandidateNotFoundException;
 import com.spring.ovs.service.NominatedCandidatesService;
@@ -19,6 +20,7 @@ import com.spring.ovs.service.NominatedCandidatesService;
 
 @ExtendWith(MockitoExtension.class)
 public class NominatedCandidatesTest
+
 {
     
 	@Mock
@@ -26,52 +28,54 @@ public class NominatedCandidatesTest
 	
 
 	@InjectMocks
-	NominatedConditesController noController;
+	NominatedCandidatesController noController;
 
 	@Test
-	public void testaddNominatedCandidate()
+	public void testAddNominatedCandidatesDetails() 
 	{
-		
 		NominatedCandidates candidate =new NominatedCandidates();
-		Mockito.when(noController.addNominatedCandidate(candidate)).thenReturn(candidate);
-		assertEquals(candidate,noController.addNominatedCandidate(candidate));
+		int candidateId = 1;
+		Mockito.when(noController.addNominatedCandidatesDetails(candidate)).thenReturn(candidateId);
+		assertEquals(candidateId,noController.addNominatedCandidatesDetails(candidate));
 	 }
+	
 	 @Test
 	 public void testGetAllNominatedCandidates()
-	 {
-	        List<NominatedCandidates> candidate=new ArrayList<NominatedCandidates>();
-	       Mockito.when(noController.getAllNominatedCandidates()).thenReturn(candidate);
-	        assertEquals(candidate,noController.getAllNominatedCandidates());
+     {
+	     List<NominatedCandidates> candidate=new ArrayList<NominatedCandidates>();
+	     Mockito.when(noController.getAllNominatedCandidates()).thenReturn(candidate);
+	     assertEquals(candidate,noController.getAllNominatedCandidates());
 
-	   }
-	 @Test
-	 public void testviewNominatedCandidatesById() throws NominatedCandidateNotFoundException 
-	 {
-		 NominatedCandidates candidate=new NominatedCandidates();
-		 int candidateId=1;
-		 Mockito.when(noController.viewNominatedCandidatesById(candidateId)).thenReturn(candidate);
-	     assertEquals(candidate,noController.viewNominatedCandidatesById(candidateId));
- 
-	 }
-	 @Test
-	 public void testupdateNominatedCandidateDetails()throws NominatedCandidateNotFoundException
-	 {
-		NominatedCandidates candidate=new NominatedCandidates();
-		int candidateId=1;
-		Mockito.when(noController. updateNominatedCandidateDetails(candidateId, candidate)).thenReturn(candidate);
-		assertEquals(candidate,noController. updateNominatedCandidateDetails(candidateId, candidate));
-
-	}
+	  }
 	 
 	 @Test
-	 public void testdeleteNominatedCandididates()throws NominatedCandidateNotFoundException
+	 public void testNominatedCandidatesById() throws NominatedCandidateNotFoundException
+	 {
+		 NominatedCandidates candidate=new NominatedCandidates();
+		 int candidateId = 1;
+		Mockito.when(noController.viewNominatedCandidatesById(candidateId)).thenReturn(candidate);
+	     assertEquals(candidate,noController.viewNominatedCandidatesById(candidateId)); 
+	 }
+	 
+	 @Test
+	 public void testUpdateNominatedCandidatesDetails()throws NominatedCandidateNotFoundException
+	 {
+		 NominatedCandidates candidate=new NominatedCandidates();
+		int candidateId = 1;
+		Mockito.when(noController.updateNominatedCandidatesDetails(candidateId, candidate)).thenReturn(candidate);
+		assertEquals(candidate,noController.updateNominatedCandidatesDetails(candidateId, candidate));
+
+	 }
+	 
+	 @Test
+	 public void testDeleteNominatedCandidates() throws NominatedCandidateNotFoundException
 	 {
 	 	
-		 int candidateId=1;
-		Mockito.when(noController.deleteNominatedCandididates(candidateId)).thenReturn(candidateId);
-	 	assertEquals(candidateId,noController.deleteNominatedCandididates(candidateId));
+	 	int candidateId = 1;
+		Mockito.when(noController.deleteNominatedCandidates(candidateId)).thenReturn(candidateId);
+	 	assertEquals(candidateId,noController.deleteNominatedCandidates(candidateId));
 	 	
 	 }
-
-
 }
+
+
