a7ff734cbff4d829f57dfc862f17d41e96fb88a0
GOWTHAMI9999 <72733925+GOWTHAMI9999@users.noreply.github.com>
2020-11-12 18:51:09 +0530

    Update RegisteredSocietyVotersController.java

--- com.spring.ovs/src/main/java/com/spring/ovs/controllers/RegisteredSocietyVotersController.java
+++ com.spring.ovs/src/main/java/com/spring/ovs/controllers/RegisteredSocietyVotersController.java
@@ -1,12 +1,8 @@
 package com.spring.ovs.controllers;
 import java.util.List;
-
-
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,8 +11,8 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.spring.ovs.dtos.RegisteredSocietyVoters;
+
 import com.spring.ovs.exceptions.VoterNotFoundException;
 import com.spring.ovs.service.RegisteredSocietyVotersService;
 
@@ -30,14 +26,24 @@ public class RegisteredSocietyVotersController
 	@Autowired
 	private RegisteredSocietyVotersService rsvService;
 	
-	
 	@PostMapping("/reg_society")
-    public @ResponseBody RegisteredSocietyVoters voterRegistration(@RequestBody RegisteredSocietyVoters voter) 
-    {
-	     return rsvService.voterRegistration(voter);
-    }
-    
+    public @ResponseBody RegisteredSocietyVoters voterRegistration(@RequestBody RegisteredSocietyVoters voter) throws VoterNotFoundException 
+{
+return rsvService.voterRegistration(voter);
+}
+
     @PutMapping("/reg_society/{voterId}")
+    /*
+     * @Author:Gowthami
+     * 
+     * @Method:updateVoterDetails()
+     * 
+     * Return type:integer
+     * 
+     * @Parameter: voter,voterId
+     * 
+     * @description: This method allows to update the voter details if you enter existing Id,that Id will be updated or if the given Id doesnot exist, it will throw an exception "No voter present to update".
+     */
     public @ResponseBody RegisteredSocietyVoters updateRegisteredSocietyVotersDetails(@PathVariable("voterId") int voterId,RegisteredSocietyVoters voter) throws VoterNotFoundException 
     {
     	
@@ -45,18 +51,51 @@ public class RegisteredSocietyVotersController
     }
     
     @DeleteMapping("/reg_society/{voterId}")
+    /*
+     * @Author:Gowthami
+     * 
+     * @Method:deleteVoter()
+     * 
+     * Return type:integer
+     * 
+     * @Parameter: voterId
+     * 
+     * @description: This method allows to delete the voter  if you enter existing Id,that Id will be deleted or if the given Id does not exist, it will throw an exception "Enter valid id to delete".
+     */
     public @ResponseBody int deleteRegisteredSocietyVoter(@PathVariable("voterId") int voterId) throws VoterNotFoundException 
     {
 	  return rsvService.deleteRegisteredSocietyVoter(voterId);
     }
     
     @GetMapping("/reg_society" )
+    /*
+     * @Author:Gowthami
+     * 
+     * @Method:viewRegisteredVoterList()
+     * 
+     * Return type:List
+     * 
+     * @Parameter: list
+     * 
+     * @description: This method allows to view the voter details if you enter existing voter,that voter details will be visible.
+     */
     public @ResponseBody List<RegisteredSocietyVoters> viewRegisteredVoterList() 
     { 
     	return rsvService.viewRegisteredVoterList();
     }
     
     @GetMapping("/reg_society/{voterId}")
+    /*
+     * @Author:Gowthami
+     * 
+     * @Method:searchByVoterID()
+     * 
+     * Return type:integer
+     * 
+     * @Parameter: voterId
+     * 
+     * @description: This method allows to search the voter id ,if you enter existing Id,that voter details can be viewed or if the given Id doesnot exist, it will throw an exception "Enter valid id to search the voter details".
+     */
     public @ResponseBody RegisteredSocietyVoters searchByVoterID(@PathVariable("voterId") int voterId) throws VoterNotFoundException 
     {
     	
@@ -64,8 +103,8 @@ public class RegisteredSocietyVotersController
 	  
     }
     
-    
+    }
 
-}
+
 
 
