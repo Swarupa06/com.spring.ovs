ffa86e24f1078af667225bef174e1092e5eff54b
sirisha696 <72748365+sirisha696@users.noreply.github.com>
2020-11-12 20:12:20 +0530

    Update CooperativeSocietyRepository.java

--- com.spring.ovs/src/main/java/com/spring/ovs/repository/CooperativeSocietyRepository.java
+++ com.spring.ovs/src/main/java/com/spring/ovs/repository/CooperativeSocietyRepository.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.spring.swagger.ovs.dtos.CooperativeSociety;
+import com.spring.ovs.dtos.CooperativeSociety;
 
 @Repository
 public interface CooperativeSocietyRepository extends JpaRepository<CooperativeSociety,Integer> {
