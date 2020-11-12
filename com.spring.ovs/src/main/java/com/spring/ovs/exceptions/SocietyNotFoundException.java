99e3258413ba51c6f50a6865bbd95867d7056fd6
sirisha696 <72748365+sirisha696@users.noreply.github.com>
2020-11-12 18:57:49 +0530

    Update SocietyNotFoundException.java

--- com.spring.ovs/src/main/java/com/spring/ovs/exceptions/SocietyNotFoundException.java
+++ com.spring.ovs/src/main/java/com/spring/ovs/exceptions/SocietyNotFoundException.java
@@ -1,12 +1,34 @@
 package com.spring.ovs.exceptions;
-public class SocietyNotFoundException extends Exception {
-	public SocietyNotFoundException(String msg) {
-		super(msg);
-		// TODO Auto-generated constructor stub
-	}
 
+
+public class SocietyNotFoundException extends RuntimeException
+{
+	
 	
 	private static final long serialVersionUID = 1L;
+	
+	public SocietyNotFoundException()
+	{
+		
+	}
 
-
+	public SocietyNotFoundException(String message)
+	{
+		super(message);
+	}
+    
+	public SocietyNotFoundException(String message,Throwable clause) 
+	{
+		super(message,clause);
+	}
+	
+	public SocietyNotFoundException(Throwable clause)
+	{
+		super(clause);
+	}
+	
 }
+
+
+
+
