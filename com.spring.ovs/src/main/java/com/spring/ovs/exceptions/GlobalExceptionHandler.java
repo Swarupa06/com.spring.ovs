b1012c26bd4aad4693ccc6b667ce25def73d5d54
Swarupa06 <74255053+Swarupa06@users.noreply.github.com>
2020-11-12 18:53:21 +0530

    Update GlobalExceptionHandler.java

--- com.spring.ovs/src/main/java/com/spring/ovs/exceptions/GlobalExceptionHandler.java
+++ com.spring.ovs/src/main/java/com/spring/ovs/exceptions/GlobalExceptionHandler.java
@@ -1,60 +1,34 @@
-package com.spring.ovs.exceptions;
+package com.spring.swagger.ovs.exceptions;
 
 import java.util.Date;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.context.request.WebRequest;
 
-@RestControllerAdvice
-public class GlobalExceptionHandler {
-
-	@ExceptionHandler(DuplicateRecordException.class)
-    @ResponseStatus(value=HttpStatus.NOT_FOUND)
-    public ResponseEntity<Object> resouceNotFoundException(DuplicateRecordException ex, WebRequest request) {
-    	ExceptionDetails exceptionDetails = new ExceptionDetails(new Date(), ex.getMessage(), request.getDescription(false));
-        return new ResponseEntity<>(exceptionDetails, HttpStatus.NOT_FOUND);
-    }
-    
-	 @ExceptionHandler(ElectionOfficerNotFoundException.class)
-	    @ResponseStatus(value=HttpStatus.NOT_FOUND)
-	    public ResponseEntity<Object> resouceNotFoundException(ElectionOfficerNotFoundException ex, WebRequest request) {
-	    	ExceptionDetails exceptionDetails = new ExceptionDetails(new Date(), ex.getMessage(), request.getDescription(false));
-	    	System.out.println("1");
-	        return new ResponseEntity<>(exceptionDetails, HttpStatus.NOT_FOUND);
-	    }
-	 @ExceptionHandler(UserNotFoundException.class)
-	    @ResponseStatus(value=HttpStatus.NOT_FOUND)
-	    public ResponseEntity<Object> resouceNotFoundException(UserNotFoundException ex, WebRequest request) {
-	    	ExceptionDetails exceptionDetails = new ExceptionDetails(new Date(), ex.getMessage(), request.getDescription(false));
-	    	System.out.println("2");
-	        return new ResponseEntity<>(exceptionDetails, HttpStatus.NOT_FOUND);
-	    }
+@ControllerAdvice
+public class GlobalExceptionHandler 
+{
+	@ExceptionHandler(ElectionOfficerNotFoundException.class)
+	public ResponseEntity<?> UserNotFoundHandling(ElectionOfficerNotFoundException exception)
+	{
+		ErrorDetails errormessage=new ErrorDetails("OfficerNotFound",exception.getMessage() ,new Date());
+		return new ResponseEntity<>(errormessage,HttpStatus.NOT_FOUND);
+	}
+	
+	
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<?> CustomValidationErrorHandling(MethodArgumentNotValidException exception)
+	{
+		ErrorDetails errormessage=new ErrorDetails("Validation Error",exception.getBindingResult().getFieldError().getDefaultMessage() ,new Date());
+		return new ResponseEntity<>(errormessage,HttpStatus.BAD_REQUEST);
+	}
+	
 	
 
-	 @ExceptionHandler(SocietyNotFoundException.class)
-	    @ResponseStatus(value=HttpStatus.NOT_FOUND)
-	    public ResponseEntity<Object> resouceNotFoundException(SocietyNotFoundException ex, WebRequest request) {
-	    	ExceptionDetails exceptionDetails = new ExceptionDetails(new Date(), ex.getMessage(), request.getDescription(false));
-	    	System.out.println("3");
-	        return new ResponseEntity<>(exceptionDetails, HttpStatus.NOT_FOUND);
-	    }
-	@ExceptionHandler(VoterNotFoundException.class)
-	    @ResponseStatus(value=HttpStatus.NOT_FOUND)
-	    public ResponseEntity<Object> resouceNotFoundException(UserNotFoundException ex, WebRequest request) {
-	    	ExceptionDetails exceptionDetails = new ExceptionDetails(new Date(), ex.getMessage(), request.getDescription(false));
-	    	System.out.println("4");
-	        return new ResponseEntity<>(exceptionDetails, HttpStatus.NOT_FOUND);
-	    }
-
-	 @ExceptionHandler(NominatedCandidateNotFoundException.class)
-	    @ResponseStatus(value=HttpStatus.NOT_FOUND)
-	    public ResponseEntity<Object> resouceNotFoundException(SocietyNotFoundException ex, WebRequest request) {
-	    	ExceptionDetails exceptionDetails = new ExceptionDetails(new Date(), ex.getMessage(), request.getDescription(false));
-	    	System.out.println("5");
-	        return new ResponseEntity<>(exceptionDetails, HttpStatus.NOT_FOUND);
-	    }
 }
+
+
