0582426bbb78dbebbe259604ab56177ec73a60b8
sirisha696 <72748365+sirisha696@users.noreply.github.com>
2020-11-12 20:09:53 +0530

    Update CooperativeSocietyService.java

--- com.spring.ovs/src/main/java/com/spring/ovs/service/CooperativeSocietyService.java
+++ com.spring.ovs/src/main/java/com/spring/ovs/service/CooperativeSocietyService.java
@@ -6,9 +6,9 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.spring.swagger.ovs.dtos.CooperativeSociety;
-import com.spring.swagger.ovs.exceptions.SocietyNotFoundException;
-import com.spring.swagger.ovs.repository.CooperativeSocietyRepository;
+import com.spring.ovs.dtos.CooperativeSociety;
+import com.spring.ovs.exceptions.SocietyNotFoundException;
+import com.spring.ovs.repository.CooperativeSocietyRepository;
 
 
 
