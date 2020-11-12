f6fd05b7197ca82a3f68c3d6c1ca4df64a6b2bc9
sirisha696 <72748365+sirisha696@users.noreply.github.com>
2020-11-12 20:11:14 +0530

    Update UserRepository.java

--- com.spring.ovs/src/main/java/com/spring/ovs/repository/UserRepository.java
+++ com.spring.ovs/src/main/java/com/spring/ovs/repository/UserRepository.java
@@ -4,7 +4,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.spring.swagger.ovs.dtos.User;
+import com.spring.ovs.dtos.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Integer> {
